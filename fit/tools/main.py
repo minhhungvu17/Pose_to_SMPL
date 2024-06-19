6575e22a71603f955bd574bbc633118369cfe2c2
minhhungvu17 <minhhung.vu17@gmail.com>
2024-06-19 03:57:21 -0700

    CLEAN fitting

--- fit/tools/main.py
+++ fit/tools/main.py
@@ -108,44 +108,43 @@ if __name__ == "__main__":
     records = []
     for root, dirs, files in os.walk(cfg.DATASET.PATH):
         for file in sorted(files):
-            record = {}
-            # if not 'baseball_swing' in file:
-            #     continue
-            # if not file == 's010_driver02.npy':
-            #     continue
             file_num += 1
-            logger.info(
-                'Processing file: {}    [{} / {}]'.format(file, file_num, len(files)))
-            data = load(args.dataset_name, os.path.join(root, file))
-            data[:,:,0] = data[:,:,0] - data[:,6:7,0]
-            data[:,:,1] = data[:,:,1] - data[:,6:7,1]
-            data[:,:,2] = data[:,:,2] - data[:,6:7,2]
-            # print('Input: ', data.shape, data[200,:,2])
-            target = torch.from_numpy(transform(args.dataset_name, data)).float()
-            print('Target: ', target.shape, target[200,:,2])
-            logger.info("target shape:{}".format(target.shape))
-            res = train(smpl_layer, target,
-                        logger, writer, device,
-                        args, cfg, meters)
-            meters.update_avg(meters.min_loss, k=target.shape[0])
-            meters.reset_early_stop()
-            logger.info("avg_loss:{:.4f}".format(meters.avg))
+            record = {}
             record['filename'] = file
-            record['loss'] = meters.avg
-            records.append(record)
-            save_params(res, smpl_layer, file, logger, args.dataset_name)
-            save_pic(res, smpl_layer, file, logger, args.dataset_name, target)
-
+            logger.info('Processing file: {}    [{} / {}]'.format(file, file_num, len(files)))
+            try:
+                # if not 'baseball_swing' in file:
+                #     continue
+                # if not file == 's010_driver02.npy':
+                #     continue
+                data = load(args.dataset_name, os.path.join(root, file))
+                data[:,:,0] = data[:,:,0] - data[:,6:7,0]
+                data[:,:,1] = data[:,:,1] - data[:,6:7,1]
+                data[:,:,2] = data[:,:,2] - data[:,6:7,2]
+                target = torch.from_numpy(transform(args.dataset_name, data)).float()
+                logger.info("target shape:{}".format(target.shape))
+                res = train(smpl_layer, target,
+                            logger, writer, device,
+                            args, cfg, meters)
+                meters.update_avg(meters.min_loss, k=target.shape[0])
+                meters.reset_early_stop()
+                logger.info("avg_loss:{:.4f}".format(meters.avg))
+                record['loss'] = meters.avg
+                records.append(record)
+                save_params(res, smpl_layer, file, logger, args.dataset_name)
+                save_pic(res, smpl_layer, file, logger, args.dataset_name, target)
+            except Exception as e:
+                print(e)
+                record['loss'] = None
             torch.cuda.empty_cache()
-            images = []
-            filenames = sorted(fn for fn in os.listdir(f'fit/output/{args.dataset_name}/picture/{file[:-4]}'))
-            for filename in filenames:
-                images.append(imageio.imread(f'fit/output/{args.dataset_name}/picture/{file[:-4]}/'+filename))
-            imageio.mimsave(f'gif/{args.dataset_name}/{file[:-4]}.gif', images, duration=0.2)
-            logger.info("Gifing finished!")
+            # images = []
+            # filenames = sorted(fn for fn in os.listdir(f'fit/output/{args.dataset_name}/picture/{file[:-4]}'))
+            # for filename in filenames:
+            #     images.append(imageio.imread(f'fit/output/{args.dataset_name}/picture/{file[:-4]}/'+filename))
+            # imageio.mimsave(f'gif/{args.dataset_name}/{file[:-4]}.gif', images, duration=0.2)
+            # logger.info("Gifing finished!")
         df = pd.DataFrame(records)
         df.to_csv(os.path.join(f'report/{args.dataset_name}', 'loss.csv'), index=False)
-            
         logger.info("Fitting finished! Average loss: {:.9f}".format(meters.avg))
 
            
